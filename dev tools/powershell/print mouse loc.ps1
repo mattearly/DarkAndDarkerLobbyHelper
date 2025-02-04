425ffb91f459955ce1c21dd4e46c9231294147c6
Matthew Early <matthewjearly@gmail.com>
2025-02-04 12:41:08 -0500

    rm unused tools, buy 4 white bandages

--- dev tools/powershell/print mouse loc.ps1
+++
@@ -1,7 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-while (1) {
-    $X = [System.Windows.Forms.Cursor]::Position.X
-    $Y = [System.Windows.Forms.Cursor]::Position.Y
-
-    Write-Host -NoNewline "`rX: $X | Y: $Y"
-}
